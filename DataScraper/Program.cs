f3601f7a608d51294303a7675fbe7d2a642b017c
“teahashani” <th64991@ubt-uni.net>
2024-12-27 00:35:10 +0100

    Initial commit

---
+++ DataScraper/Program.cs
@@ -0,0 +1,13 @@
+﻿using DataScraper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using DataScraper;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+    }
+}
+

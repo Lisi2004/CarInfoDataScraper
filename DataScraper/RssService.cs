3304aab474029e3e6c38e54930f9c03cd0c22b77
Lisi2004 <lismaloku@gmail.com>
2024-12-26 23:35:05 +0100

    Add project files.

---
+++ DataScraper/RssService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataScraper
+{
+    public class RssService
+    {
+        private readonly AppDbContext _dbContext;
+
+        public RssService(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+    }
+}

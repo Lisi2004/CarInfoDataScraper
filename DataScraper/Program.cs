a531c7a6889795d582b32ff67989232cc6875a97
Lisi2004 <lismaloku@gmail.com>
2024-12-29 18:51:47 +0100

    Added aditional logic to RssService and set up project in Program.cs

--- DataScraper/Program.cs
+++ DataScraper/Program.cs
@@ -8,6 +8,19 @@ public class Program
 {
     public static async Task Main(string[] args)
     {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(@"./appsettings.json")
+            .Build();
+
+        var serviceProvider = new ServiceCollection()
+            .AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DevConnection")))
+            .AddTransient<RssService>()
+            .BuildServiceProvider();
+
+        var rssService = serviceProvider.GetRequiredService<RssService>();
+        await rssService.ExecuteRss();
     }
 }
 
--- DataScraper/RssService.cs
+++ DataScraper/RssService.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HtmlAgilityPack;
+using System.Web;
+using System.Xml.Linq;
 
 namespace DataScraper
 {
@@ -15,53 +13,149 @@ namespace DataScraper
             _dbContext = dbContext;
         }
 
-    public async Task ExecuteRss()
-{
-    string rssFeedUrl = "https://www.veturaneshitje.com/home/rss";
-    var carUrls = await GetCarUrlsFromRssFeed(rssFeedUrl);
-
-    var cars = new List<Car>();
-    foreach (var carUrl in carUrls)
-    {
-        try
+        public async Task ExecuteRss()
         {
-            var car = await GetCarDetailsFromUrl(carUrl);
-            if (car != null)
+            string rssFeedUrl = "https://www.veturaneshitje.com/home/rss";
+            var carUrls = await GetCarUrlsFromRssFeed(rssFeedUrl);
+
+            var cars = new List<Car>();
+            foreach (var carUrl in carUrls)
             {
-                cars.Add(car);
+                try
+                {
+                    var car = await GetCarDetailsFromUrl(carUrl);
+                    if (car != null)
+                    {
+                        cars.Add(car);
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine($"Error processing URL {carUrl}: {ex.Message}");
+                }
+            }
+
+            await SaveCarsToDb(cars);
+        }
+        public async Task<List<string>> GetCarUrlsFromRssFeed(string rssFeedUrl)
+        {
+            var carUrls = new List<string>();
+            var httpClient = new HttpClient();
+            var rssData = await httpClient.GetStringAsync(rssFeedUrl);
+
+            var rssXml = XDocument.Parse(rssData);
+            foreach (var item in rssXml.Descendants("item"))
+            {
+                var link = item.Element("link")?.Value?.Trim();
+                if (!string.IsNullOrEmpty(link))
+                {
+                    carUrls.Add(link);
+                }
+            }
+
+            return carUrls;
+        }
+
+        public async Task<Car> GetCarDetailsFromUrl(string url)
+        {
+            try
+            {
+                var httpClient = new HttpClient();
+                var response = await httpClient.GetAsync(url);
+
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine($"Failed to retrieve data from URL: {url}. Status code: {response.StatusCode}");
+                    return null;
+                }
+
+                var html = await response.Content.ReadAsStringAsync();
+                var htmlDocument = new HtmlDocument();
+                htmlDocument.LoadHtml(html);
+                var carNode = htmlDocument.DocumentNode.SelectSingleNode("//table[contains(@class, 'table-car-specifications')]");
+
+                if (carNode != null)
+                {
+
+                    var title = HttpUtility.HtmlDecode(htmlDocument.DocumentNode.SelectSingleNode("//h1[contains(@class, 'no-margin')]")?.InnerText.Trim());
+                    var price = HttpUtility.HtmlDecode(htmlDocument.DocumentNode.SelectSingleNode("//h3[contains(@class, 'text-orange price lead no-margin')]/strong")?.InnerText.Trim());
+                    var customs = HttpUtility.HtmlDecode(carNode.SelectSingleNode(".//td[contains(., 'Dogana')]/../following-sibling::tr[1]/td[1]")?.InnerText.Trim());
+                    var registration = HttpUtility.HtmlDecode(carNode.SelectSingleNode(".//td[contains(., 'Dogana')]/../following-sibling::tr[1]/td[2]")?.InnerText.Trim());
+                    var vehicleType = HttpUtility.HtmlDecode(carNode.SelectSingleNode(".//span[text()='Viti']/../../following-sibling::tr[1]/td[1]")?.InnerText.Trim());
+                    var year = HttpUtility.HtmlDecode(carNode.SelectSingleNode(".//span[text()='Viti']/../../following-sibling::tr[1]/td[2]")?.InnerText.Trim());
+                    var fuelType = HttpUtility.HtmlDecode(carNode.SelectSingleNode(".//span[text()='Karburanti']/../../following-sibling::tr[1]/td[1]")?.InnerText.Trim());
+                    var transmission = HttpUtility.HtmlDecode(carNode.SelectSingleNode(".//span[text()='Marshi']/../../following-sibling::tr[1]/td[2]")?.InnerText.Trim());
+                    var color = HttpUtility.HtmlDecode(carNode.SelectSingleNode(".//span[text()='Ngjyra']/../../following-sibling::tr[1]/td[1]")?.InnerText.Trim());
+                    var seats = HttpUtility.HtmlDecode(carNode.SelectSingleNode(".//span[text()='Ulese']/../../following-sibling::tr[1]/td[2]")?.InnerText.Trim());
+                    var engineSize = HttpUtility.HtmlDecode(carNode.SelectSingleNode(".//span[text()='Kubikazha']/../../following-sibling::tr[1]/td[1]")?.InnerText.Trim());
+                    var mileage = HttpUtility.HtmlDecode(carNode.SelectSingleNode(".//span[text()='Kilometrazhi']/../../following-sibling::tr[1]/td[2]")?.InnerText.Trim());
+
+
+                    var linkId = ExtractIdFromUrl(url);
+
+                    return new Car
+                    {
+                        Title = title,
+                        Price = price,
+                        Customs = customs,
+                        Registration = registration,
+                        VehicleType = vehicleType,
+                        Year = year,
+                        FuelType = fuelType,
+                        Transmission = transmission,
+                        Color = color,
+                        Seats = seats,
+                        EngineSize = engineSize,
+                        Mileage = mileage,
+
+                        LinkId = linkId
+                    };
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Error fetching data from URL: {url}");
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+            }
+
+            return null;
+        }
+
+        public async Task SaveCarsToDb(List<Car> cars)
+        {
+            try
+            {
+                _dbContext.Cars.AddRange(cars);
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error saving cars to the database: {ex.Message}");
             }
         }
-        catch (Exception ex)
+
+        private string ExtractIdFromUrl(string url)
         {
-           
-            Console.WriteLine($"Error processing URL {carUrl}: {ex.Message}");
+
+            var segments = url.Split('/');
+
+
+            if (segments.Length >= 4)
+            {
+                return segments[4];
+            }
+            else if (segments.Length < 4)
+            {
+                return segments[3];
+            }
+
+            return string.Empty;
         }
     }
-
-    await SaveCarsToDb(cars);
 }
-
-
-    }
-}
-```
-public async Task<List<string>> GetCarUrlsFromRssFeed(string rssFeedUrl)
-{
-    var carUrls = new List<string>();
-    var httpClient = new HttpClient();
-    var rssData = await httpClient.GetStringAsync(rssFeedUrl);
-
-    var rssXml = XDocument.Parse(rssData);
-    foreach (var item in rssXml.Descendants("item"))
-    {
-        var link = item.Element("link")?.Value?.Trim();
-        if (!string.IsNullOrEmpty(link))
-        {
-            carUrls.Add(link);
-        }
-    }
-
-    return carUrls;
-}
-
-```
